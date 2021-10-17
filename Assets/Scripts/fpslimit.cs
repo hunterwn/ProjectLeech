e7bac8f5e5ecc7d53fb1215133e13051a79ca3b3
Alexander Melendez <60493143+Alexander-Melendez@users.noreply.github.com>
2021-10-17 19:10:22 -0400

    added enemy

---
+++ Assets/Scripts/fpslimit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpslimit : MonoBehaviour
+{
+
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coroutine = FPSlim(0);
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FPSlim(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+}
