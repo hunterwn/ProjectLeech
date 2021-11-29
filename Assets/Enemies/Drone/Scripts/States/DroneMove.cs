23fafdda2f292d81d50061b061c408e8d052d251
Alexander Melendez <60493143+Alexander-Melendez@users.noreply.github.com>
2021-11-28 20:00:07 -0500

    Drone Outline + Assets + InvisWalls

---
+++ Assets/Enemies/Drone/Scripts/States/DroneMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMove : DroneState
+{
+    void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
