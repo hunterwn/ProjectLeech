99106e0567f76ef9bf49102b13b672363c6cba07
john <john.n.pham2000@gmail.com>
2021-11-28 19:22:44 -0500

    Added health system

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     public int health;
-    public int numOfBars;
+    public int currentHealth;
 
     public Image[] bars;
     public Sprite filledBar;
@@ -14,19 +14,30 @@ public class Health : MonoBehaviour
 
     void Update()
     {
-        if (health > numOfBars)
-            health = numOfBars;
+        if (health > currentHealth)
+            health = currentHealth;
+
         for (int i = 0; i < bars.Length; i++)
         {
             if (i < health)
+            {
                 bars[i].sprite = filledBar;
+            }
             else
+            {
                 bars[i].sprite = emptyBar;
+            }
 
             if (i < numOfBars)
+            {
                 bars[i].enabled = true;
+            }
             else
+            {
                 bars[i].enabled = false;
+
+            }
+
         }
     }
 }
