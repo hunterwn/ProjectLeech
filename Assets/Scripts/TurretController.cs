dbb5cbbdb8e2ebaffa4114f4e48ea6c7f50dafcb
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-12-01 00:49:39 -0500

    Merge ps/level1

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour {
+  private enum TurretType { Timer, Raycast };
+
+  [SerializeField]
+  private TurretType type;
   [SerializeField]
   private GameObject laser;
   [SerializeField]
   private GameObject laserBeam;
+  [SerializeField]
+  private GameObject aimLaser;
   [SerializeField]
   private GameObject laserLight;
   [SerializeField]
@@ -19,28 +25,55 @@ public class TurretController : MonoBehaviour {
   private float shootDuration = 2f;
   [SerializeField]
   private float rechargeDuration = 2f;
+  [SerializeField]
+  private float aimLaserWidth = 0.02f;
 
-  private bool turretDown = false;
-  private float secondsDown = 1f;
+  private bool turretDown;
+  private float secondsDown;
   private Coroutine shootCoroutine;
   private AudioSource soundEffect;
   private Light chargingLight;
   private float endingLightIntensity;
+  private LineRenderer aimLaserRenderer;
+  private float aimLaserCurWidth;
+  private bool isShooting = false;
 
   private GameObject[] lights = new GameObject[50];
 
   private void Start() {
     this.soundEffect = GetComponent<AudioSource>();
     this.chargingLight = this.laserLight.GetComponent<Light>();
+    this.aimLaserRenderer = this.aimLaser.GetComponent<LineRenderer>();
     this.endingLightIntensity = this.chargingLight.intensity;
     this.chargingLight.intensity = 0f;
+    this.aimLaserCurWidth = 0f;
 
     SetupLaser();
-    this.shootCoroutine = StartCoroutine(ShootLaser());
+
+    if (this.type == TurretType.Timer) {
+      this.shootCoroutine = StartCoroutine(ShootLaser());
+    }
   }
 
   private void Update() {
+    // Check if player is in view of the turret
+    if (this.type == TurretType.Raycast && !this.isShooting) {
+      RaycastHit hit;
+      Ray laserRay = new Ray(this.laser.transform.position, transform.forward);
+      Physics.Raycast(laserRay, out hit, laserLayerMask.value);
+      if (hit.collider.tag == "Player") {
+        this.shootCoroutine = StartCoroutine(ShootLaser());
+      }
+    }
+
+    // Charge up animation
     if (this.laserLight.activeSelf) {
+      // Aiming Laser
+      this.aimLaserCurWidth += Time.deltaTime * this.aimLaserWidth / this.windUpTime;
+      this.aimLaserCurWidth = Mathf.Clamp(this.aimLaserCurWidth, 0, this.aimLaserWidth);
+      this.aimLaserRenderer.widthCurve = AnimationCurve.Constant(0, 1, this.aimLaserCurWidth);
+
+      // Charging Light
       float lightIntensity = this.chargingLight.intensity;
       lightIntensity += Time.deltaTime * this.endingLightIntensity / this.windUpTime;
       lightIntensity = Mathf.Clamp(lightIntensity, 0, endingLightIntensity);
@@ -62,6 +95,8 @@ public class TurretController : MonoBehaviour {
     this.soundEffect.Stop();
     this.laser.SetActive(false);
     this.laserLight.SetActive(false);
+    this.aimLaser.SetActive(false);
+    this.aimLaserCurWidth = 0;
   }
 
   private void SetupLaser() {
@@ -71,6 +106,7 @@ public class TurretController : MonoBehaviour {
     float laserLength = hit.distance;
 
     this.laserBeam.transform.localScale = Vector3.forward * laserLength;
+    this.aimLaser.transform.localScale = Vector3.forward * laserLength;
     int numLights = Mathf.RoundToInt(laserLength / lightGapDistance);
     for (int i = 0; i < lights.Length; i++) {
       if (i >= numLights) {
@@ -88,6 +124,8 @@ public class TurretController : MonoBehaviour {
 
   IEnumerator ShootLaser() {
     while (true) {
+      this.isShooting = true;
+
       // Check if Turret is disabled
       if (this.turretDown) {
         this.turretDown = false;
@@ -98,8 +136,11 @@ public class TurretController : MonoBehaviour {
       // Laser wind up
       this.soundEffect.Play();
       this.laserLight.SetActive(true);
-      this.chargingLight.intensity = 0;
+      this.aimLaser.SetActive(true);
+      this.chargingLight.intensity = 0f;
+      this.aimLaserCurWidth = 0f;
       yield return new WaitForSeconds(this.windUpTime);
+      this.aimLaser.SetActive(false);
       this.laserLight.SetActive(false);
 
       // Laser Fire
@@ -111,6 +152,11 @@ public class TurretController : MonoBehaviour {
       this.laser.SetActive(false);
 
       yield return new WaitForSeconds(this.rechargeDuration);
+
+      this.isShooting = false;
+      if(this.type != TurretType.Timer) {
+        break;
+      }
     }
   }
 }
