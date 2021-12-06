2ca8439b8e76fd726fa99624a45cf9995dfb385f
hunterwn <hunterwnorman@gmail.com>
2021-12-06 17:24:02 -0500

    Improved grate collisions

--- Assets/Player/KeepInbounds.cs
+++ Assets/Player/KeepInbounds.cs
@@ -10,6 +10,11 @@ public class KeepInbounds : MonoBehaviour
     private Vector3 pushDirection;
     float pushForce = 2.0f;
     private void OnCollisionEnter(Collision collision) {
+        //check for grates
+        if(collision.gameObject.tag == "Grate")
+        {
+            return;
+        }
         if(collision.gameObject.layer == 8)
         {
             pushDirection = player.velocity * -1.0f;
